25013a85e769815af5af5f0ef3d6e9027158f74d
zeeshan <zeeshanbutt223@gmail.com>
2023-05-04 18:32:22 +0500

    rewatch implementation

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,6 @@
 export default {
     port: getEnv('PORT', '3000'),
     host: getEnv('HOST', 'localhost'),
+    rewatch_token: getEnv('REWATCH_TOKEN', ''),
+    rewatch_uri: getEnv('REWATCH_URI', ''),
 };
